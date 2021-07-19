d9a6b9baba2e245353e9d141501c804b5edae97c
Peter Pak <ppak10@gmail.com>
2021-07-18 20:53:38 -0500

    Adds MaterialUI

--- src/symbols/components/Input.tsx
+++ src/symbols/components/Input.tsx
@@ -4,14 +4,25 @@
  */
 
 // Node Modules
+import {FormControl, InputLabel, MenuItem, Select} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 import {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 
 
+// Hooks
 import {useSymbolsAPI} from '../hooks';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
 
 export default function Input() {
   // Hooks
+  const classes = useStyles();
   const {get} = useSymbolsAPI();
   const symbolsAll = useSelector(({symbols}) => symbols.all);
 
@@ -19,13 +30,16 @@ export default function Input() {
     get();
   }, [get]);
 
-  const symbolsJSX = Object.keys(symbolsAll).map((symbol) => (
-    <li key={symbol}>{symbol}</li>
+  const menuItemsJSX = Object.keys(symbolsAll).map((symbol) => (
+    <MenuItem key={symbol} value={symbol}>{symbol}</MenuItem>
   ));
 
   return (
-    <ul>
-      {symbolsJSX}
-    </ul>
+    <FormControl className={classes.formControl}>
+      <InputLabel>Symbol</InputLabel>
+      <Select>
+        {menuItemsJSX}
+      </Select>
+    </FormControl>
   );
 }
